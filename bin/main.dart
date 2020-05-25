15afbcd396bc76b88842325c961a6a1810cc13aa
Eyad Web Developmnet <30596418+development1soft@users.noreply.github.com>
2020-05-25 18:34:16 +0300

    Dart Level 2 Started Successfully

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:Dart_Mastering_Level_2/Dart_Mastering_Level_2.dart' as Dart_Mastering_Level_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${Dart_Mastering_Level_2.calculate()}!');
+}
---
+++ test/Dart_Mastering_Level_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:Dart_Mastering_Level_2/Dart_Mastering_Level_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
